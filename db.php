d31abc99c8df771be19fb1b0cab0584091784d11
Vijay Singh Parmar <119858909+VijaySParmar1@users.noreply.github.com>
2023-05-05 11:08:57 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$connection= mysqli_connect('localhost', 'u136973163_website', '$V1Qy&4mN', 'u136973163_website');
+if(!$connection){
+    //echo "We are connected";
+    die("Database connection failed");
+}
+?>
